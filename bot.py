3bdddb81ee0ea1bc7f3a09b0641f2050122cc19c
Ivan <76881741+666Ivan@users.noreply.github.com>
2021-06-25 19:32:26 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+import time
+import pyautogui as p
+time.sleep(3)
+a = 1
+while True:
+	print(a)
+	a = a +!work
+	!slut
+	 1
+	p.typewrite('!work')
+	time.sleep(1)
+	p.press('enter')
+	p.typewrite('!slut')
+	time.sleep(6)
+	p.press('enter')
+	time.sleep(60)
